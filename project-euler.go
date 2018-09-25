8ca5becd1ad1c8d92fe62c2319de75f523ded936
Charlie Copprell <chowdy@gmail.com>
2018-09-25 22:56:28 +0900

    god, golang is sooooo C-like. sexy, but tedious...

---
+++ project-euler.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"plugin"
+)
+
+func main() {
+
+	// First command line argument is the problem number
+	var problem = os.Args[1]
+
+	// Prepend the problem number with zeroes if necessary
+	for i := len(problem); i < 3; i++ {
+		problem = "0" + problem
+	}
+
+	// Open the built plugin corresopnding to the command line arg
+	solution, err := plugin.Open("solution" + problem + ".so")
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the solution from the plugin we opened
+	solutionFunc, err := solution.Lookup("Run")
+	if err != nil {
+		panic(err)
+	}
+
+	// Run the solution and print the result
+	answer := solutionFunc.(func() string)()
+	fmt.Println(answer)
+}
