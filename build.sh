6a8b6e024b934ac64af5546553c3225e5bf7f655
Charlie Copprell <chowdy@gmail.com>
2018-09-28 00:42:55 +0900

    six

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# I don't know if this is idiomatic (it's probably not), but I'm new
+# TODO: This better lol
 find solution*.go | xargs -L 1 go build -buildmode=plugin
---
+++ solution006.go
@@ -0,0 +1,33 @@
+package main
+
+import "strconv"
+
+/*
+ * The sum of the squares of the first ten natural numbers is,
+ * 		1^2 + 2^2 + ... + 10^2 = 385
+ *
+ * The square of the sum of the first ten natural numbers is,
+ * 		(1 + 2 + ... + 10)^2 = 55^2 = 3025
+ *
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+ * 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the
+ * sum.
+ */
+
+func Solution006() string {
+
+	MAX := 100
+	sumOfSquares := 0
+	squareOfSum := 0
+
+	for i := 1; i <= MAX; i++ {
+		sumOfSquares += i*i
+		squareOfSum += i
+	}
+
+	squareOfSum *= squareOfSum
+
+	return strconv.Itoa(squareOfSum - sumOfSquares)
+}
